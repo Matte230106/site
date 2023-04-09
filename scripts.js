9b35b64ef21182a2bdb349d27c16985bb7fc2ea5
Matte230106 <130281943+Matte230106@users.noreply.github.com>
2023-04-09 14:47:21 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+```javascript
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    alert('Grazie per averci contattato! Ti risponderemo al piÃ¹ presto.');
+});
+```
